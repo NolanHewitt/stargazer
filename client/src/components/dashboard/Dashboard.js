3cc8fa75d4e28cfe5bd1c5ce941ff039aedd9c29
Nolan Hewitt <nolanhewitt@aol.com>
2019-10-17 13:15:00 -0500

    location

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,19 +1,18 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext,} from "react";
 import PropTypes from "prop-types";
-import {Link} from "react-router-dom";
 import $ from 'jquery';
+import axios from 'axios';
+import cheerio from 'cheerio';
 
 import { AuthContext } from "../../auth/auth";
 
 export default function Dashboard() {
   const { user, logoutUser } = useContext(AuthContext);
 
-  const [myGames, setMyGames] = useState([]);
 
   let city;
   let state;
-  let country;
-  let continent;
+
   let latitude;
   let longitude;
 
@@ -28,7 +27,6 @@ export default function Dashboard() {
     console.log("Longitude: " + data.geobyteslongitude);
     city = data.geobytescity;
     state = data.geobytesregion;
-    country = data.geobytescountry;
     latitude = data.geobyteslatitude;
     longitude = data.geobyteslongitude;
     
@@ -38,7 +36,24 @@ export default function Dashboard() {
     document.getElementById("coordinates").innerHTML = latitude + " " + longitude;
 
     let url = "https://in-the-sky.org/skymap2.php?no_cookie=1&latitude=" +latitude+ "&longitude=" +longitude+ "&timezone=-5.00&year=2019&month=10&day=16&hour=10&min=28&PLlimitmag=0&zoom=160&ra=10.91694&dec=44.97997"
-    document.getElementById("URL").innerHTML = url;
+    console.log("Url: "+url);
+
+
+    axios.get(url).then(function(response) {
+    let $ = cheerio.load(response.data);
+    console.log($);
+
+    // Grab image
+    $("div.PLhost.position_relative").each(function(i, element) {
+      // Save an empty result object
+     let result = {};
+      // Save the text and link of each a from each h3
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+    });
+  });
+
   });
 
   return (
@@ -76,48 +91,6 @@ export default function Dashboard() {
         </div>
       </div>
     </div>
-      <div>
-        <button
-          style={{
-            width: "150px",
-            borderRadius: "3px",
-            letterSpacing: "1.5px",
-            marginTop: "1rem"
-          }}
-          onClick={e => {
-            e.preventDefault();
-            console.log("user", user, {
-              x: user.id
-            });
-            fetch("/api/v1/games", {
-              method: "POST",
-              body: JSON.stringify({
-                x: user.id
-              }),
-              headers: {
-                "Content-Type": "application/json"
-              }
-            })
-              .then(res => res.json())
-              .then(res => console.log(res));
-          }}
-          className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-        >
-          Start Game
-        </button>
-        <div>
-          <h1>GAMES</h1>
-          <div>
-            {myGames.map(game => (
-              <div>
-                <Link to={`/games/${game._id}`}>
-                  {game._id} => {game.game}
-                </Link>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
     </>
   );
 }
