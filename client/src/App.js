0fa86e0afaf2ce8cb37997f1c8d335ddc24ce4eb
Nolan Hewitt <nolanhewitt@aol.com>
2019-10-17 10:54:17 -0500

    addedbasicauth

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
+import Game from "./components/game/Game";
+
+import { Auth } from "./auth/auth";
+
+import "./App.css";
+
+export default function() {
+  return (
+    <Auth>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Switch>
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          </Switch>
+        </div>
+      </Router>
+    </Auth>
+  );
+}
