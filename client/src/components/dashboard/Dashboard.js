523becc5e54c6f6d015a795e4b23ad75a00faed3
Sonny D <damicose14@gmail.com>
2019-10-18 12:41:20 -0500

    laying basic groundwork for comment history component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,7 +2,6 @@ import React, { useContext,} from "react";
 import PropTypes from "prop-types";
 import $ from 'jquery';
 import Locator from '../locator/locator'
-
 import Skywatch from "../skywheel/watch"
 
 import { AuthContext } from "../../auth/auth";
--- client/src/components/userHistory/userHistory.js
+++ client/src/components/userHistory/userHistory.js
@@ -1,61 +1,22 @@
-import React, { useState, useEffect } from "react";
+// Working with Section 20-11 as a template for Posting and Getting comments via Mongo
 
+// Import VVV this VVV into dashboard
+// import { History, HistoryItem } from "../components/userHistory";
+
+
+import React from "react";
 import "./userHistory.css";
 
-function setCharAt(str,index,chr) {
-  if(index > str.length-1) return str;
-  return str.substr(0,index) + chr + str.substr(index+1);
-}
+// This file exports both the List and ListItem components ==> Comments and CommentAll resp.
 
-function getGame(gameId, cb) {
-  fetch(`/api/v1/games/${gameId}`)
-  .then(res => res.json())
-  .then(res => {
-    console.log("PARTY =>", res);
-    cb(res.game);
-  });
-}
-
-export default function Game({ match }) {
-  const [game, setGame] = useState(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      getGame(match.params.gameId, setGame)
-    }, 1000);
-  });
-
+export function History({ children }) {
   return (
-    <div className="game-container">
-      {game &&
-        game.game.split("").map((s, i)=> (
-          <div
-            key={i}
-            className="game-box"
-            onClick={s !== "_" ? () => {} : () =>
-              fetch(`/api/v1/games/${game._id}`, {
-                method: "PUT",
-                body: JSON.stringify({
-                  // x: game.x,
-                  // o: game.o,
-                  // currentTurn: game.currentTurn,
-                  // game: game.game,
-                  ...game, // same as above,
-                  game: setCharAt(game.game, i, game.currentTurn)
-                }),
-                headers: {
-                  "Content-Type": "application/json"
-                }
-              })
-                .then(res => {
-                  console.log("*********", res)
-                  return getGame(match.params.gameId, setGame)
-                })
-            }
-          >
-            {s}
-          </div>
-        ))}
+    <div className="list-overflow-container">
+      <ul className="list-group">{children}</ul>
     </div>
   );
+}
+
+export function HistoryItem({ children }) {
+  return <li className="list-group-item">{children}</li>;
 }
