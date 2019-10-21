b07835892339107149bf7d034b4c8d2c92b32877
Daniel Merkl <dan.merkl.90@gmail.com>
2019-10-21 14:29:58 -0500

    weather

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,10 +1,11 @@
-import React, { useContext,} from "react";
+import React, { useContext, } from "react";
 import PropTypes from "prop-types";
 import Locator from '../locator/locator'
 import Skywatch from "../skywheel/watch"
 import { History, HistoryItem } from "../userHistory";
 import { AuthContext } from "../../auth/auth";
-import Comment from "../comment";
+import Comment from "../Comment/index";
+import Weather from "../Weather/index";
 
 export default function Dashboard() {
   const { user, logoutUser } = useContext(AuthContext);
@@ -13,51 +14,51 @@ export default function Dashboard() {
 
   return (
     <>
-    <Locator/>
-    <Skywatch/>
-
-    <div style={{ height: "75vh" }} className="container valign-wrapper">
-      <div className="row">
-        <div className="landing-copy col s12 center-align">
-          <h4>
-            <b>Hey there,</b> {user.name.split(" ")[0]}
-            <p className="flow-text grey-text text-darken-1">
-              You are logged into a full-stack{" "}
-              <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
+      <Locator />
+      <Skywatch />
+      <Weather />
+      <div style={{ height: "75vh" }} className="container valign-wrapper">
+        <div className="row">
+          <div className="landing-copy col s12 center-align">
+            <h4>
+              <b>Hey there,</b> {user.name.split(" ")[0]}
+              <p className="flow-text grey-text text-darken-1">
+                You are logged into a full-stack{" "}
+                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
             </p>
 
-            <p id="location"></p>
-            <p id="coordinates"></p>
-            <p id="URL"></p>
+              <p id="location"></p>
+              <p id="coordinates"></p>
+              <p id="URL"></p>
 
 
-          </h4>
-          <button
-            style={{
-              width: "150px",
-              borderRadius: "3px",
-              letterSpacing: "1.5px",
-              marginTop: "1rem"
-            }}
-            onClick={e => {
-              e.preventDefault();
-              logoutUser();
-            }}
-            className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-          >
-            Logout
+            </h4>
+            <button
+              style={{
+                width: "150px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px",
+                marginTop: "1rem"
+              }}
+              onClick={e => {
+                e.preventDefault();
+                logoutUser();
+              }}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+            >
+              Logout
           </button>
 
 
+          </div>
+          <Comment></Comment>
+          <History>
+            <HistoryItem>
+              {/* Comment History to go here at some point! */}
+            </HistoryItem>
+          </History>
         </div>
-        <Comment></Comment>
-        <History>
-<HistoryItem>
-  {/* Comment History to go here at some point! */}
-</HistoryItem>
-</History>
       </div>
-    </div>
     </>
   );
 }
