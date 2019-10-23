7eec5dbf504a003aaaad4e3c6ab89395be6f47e5
Jack Meuwissen <jmeuwissen42@gmail.com>
2019-10-23 15:34:44 -0500

    tones are stored in a string seperated by spaces

--- controllers/commentControl.js
+++ controllers/commentControl.js
@@ -13,23 +13,67 @@ module.exports = {
                 'timestamp': -1
             })
             .limit(7)
-            .then( dbModel => res.json(dbModel))
+            .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
     create: function (req, res) {
+        console.log(req.body)
+        const toneAnalyzer = new ToneAnalyzerV3({
+            version: '2017-09-21',
+            authenticator: new IamAuthenticator({
+                apikey: 'Pfsyl8Ttq6wGDbcPtS0KuLXNQqb4IhdnLPyAuW57Xmcl',
+            }),
+            url: 'https://gateway.watsonplatform.net/tone-analyzer/api',
+        });
+
+        const toneParams = {
+            toneInput: { 'text': req.body.text },
+            contentType: 'application/json',
+          };
 
 
+          //watson tone call
+          toneAnalyzer.tone(toneParams)
+          .then(toneAnalysis => {
+              let toneString = "";
+            console.log(JSON.stringify(toneAnalysis, null, 2));
+            console.log(JSON.stringify(toneAnalysis.result, null, 2));
+            console.log(JSON.stringify(toneAnalysis.result.document_tone, null, 2));
+            console.log(JSON.stringify(toneAnalysis.result.document_tone.tones, null, 2));
 
-        db.Comment
+            const tones = toneAnalysis.result.document_tone.tones;
+            //Adding each tone to the tone string
+            if (tones.length === 0) {
+                toneString = "No tones detected";
+            } else {
+                tones.forEach(tone =>{
+                    toneString += (tone.tone_name + " ");
+                })
+            }
+
+            req.body.tone = toneString;
+            //now we insert the comment into the database with the comment tone
+            db.Comment
             .create(req.body)
-            .then(function(dbComment) {
-// db.User.id = db.Comment.id?
-                return db.User.findOneAndUpdate({}, { $push: { comments: dbComment._id } }, { new: true });
-              })
+            .then(function (dbComment) {
+                // db.User.id = db.Comment.id?
+                return db.User.findOneAndUpdate({}, {
+                    $push: {
+                        comments: dbComment._id
+                    }
+                }, {
+                    new: true
+                });
+            })
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
-            console.log(db.User.findOneAndUpdate({}).schema.paths.name.SchemaString);
-            console.log();
+        console.log(db.User.findOneAndUpdate({}).schema.paths.name.SchemaString);
+        console.log();
+          })
+          .catch(err => {
+            console.log('error:', err);
+          });
+
     },
     remove: function (req, res) {
         db.Comment
